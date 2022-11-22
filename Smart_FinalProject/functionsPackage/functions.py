b6453b1ea94f7508dffccc4321fa2e45ac1bf2fc
Kevin Wilson <kevinw@interlink.com>
2022-11-22 10:51:49 -0500

    removing duplicate files and adding in logic to grab location.

--- Smart_FinalProject/functionsPackage/functions.py
+++ Smart_FinalProject/functionsPackage/functions.py
@@ -3,3 +3,29 @@ Created on Nov 22, 2022
 
 @author: KevinWilson
 '''
+import os, json
+
+
+f = open('english.txt','r')
+test = open('EncryptedGroupHints.json')
+file = f.readlines()
+
+print(file)
+
+#file.replace('\n','')
+
+
+
+parsed_json = json.load(test)
+
+print(parsed_json)
+json = (parsed_json['Smart'])
+for num in json:
+    print(file[int(num)])
+
+'''
+text = []
+for item in file:
+        print(json[int(item)])
+print(text)
+'''
